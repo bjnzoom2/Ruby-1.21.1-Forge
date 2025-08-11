d543855dce00eed409be61a6a2d8b2347f9551ca
bjnzoom2 <bjnzoom2@gmail.com>
2025-08-11 19:51:17 +0800

    Initial Commit

---
+++ src/main/java/net/bjnzoom2/ruby/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.bjnzoom2.ruby.item;
+
+import net.bjnzoom2.ruby.Ruby;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Ruby.MOD_ID);
+
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_RUBY = ITEMS.register("raw_ruby",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
