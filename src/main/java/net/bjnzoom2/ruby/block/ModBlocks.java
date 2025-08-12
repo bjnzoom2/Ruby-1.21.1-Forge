7fbd34d6aec7d63b18007fbe25d46e9270facc8e
bjnzoom2 <bjnzoom2@gmail.com>
2025-08-12 20:56:05 +0800

    Initial Commit

--- src/main/java/net/bjnzoom2/ruby/block/ModBlocks.java
+++ src/main/java/net/bjnzoom2/ruby/block/ModBlocks.java
@@ -17,7 +17,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Ruby.MOD_ID);
 
-    public static final RegistryObject<Block> RUBY_BLOCK = registerBlock();
+    public static final RegistryObject<Block> RUBY_BLOCK = registerBlock("ruby_block");
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
